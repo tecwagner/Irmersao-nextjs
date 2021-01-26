217792b181d2e1848ef1a21fcdc008ca91138112
Wagner <wagneroliveirarodrigues@gmail.com>
2021-01-26 13:30:16 -0300

    Ajuste na resolução do layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,37 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import db from "../db.json";
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    color: ${({ theme }) => theme.colors.contrastText};
   }
-`
-//Subistituir pelo db.theme
-// const theme = {
-//   colors: {
-//     primary: '#0070f3',
-//   },
-// }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
 const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import db from '../db.json'
 import Widget from '../src/components/Widget'
 import Footer from '../src/components/Footer'
